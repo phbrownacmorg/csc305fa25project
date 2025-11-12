9ec2c47c19c1db667dc77eb8abb70744dcdaa0c1
Peter Brown <phbrown@acm.org>
2025-11-12 15:46:50 -0500

    End of class 11/12

--- 305demo/routes/index.js
+++ 305demo/routes/index.js
@@ -5,17 +5,28 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   console.log('req.query (GET): '+JSON.stringify(req.query));
   req.body = req.query; // Now behaves very much like POST
-  showPage(req, res, next);
+  listTerms(req, res, next);
 //  res.render('index', { title: '305demo' });
 });
 
 /* POST home page. */
 router.post('/', function(req, res, next) {
   console.log('req.body (POST): '+JSON.stringify(req.body));
-  showPage(req, res, next);
+  listTerms(req, res, next);
 });
 
-function showPage(req, res, next) {
+function listTerms(req, res, next) {
+  let sql = 'SELECT distinct OffTerm, OffYear from Offering order by OffYear;'
+  req.app.locals.db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      req.app.locals.termslist = rows;
+      listFaculty(req, res, next);
+  })
+}
+
+function listFaculty(req, res, next) {
   if (req.body.HR_page) {
     console.log('HR page');
     // res.render('HR_page', { title: 'HR page' });
@@ -25,18 +36,23 @@ function showPage(req, res, next) {
       if (err) {
         throw err;
       }
-      req.app.locals.db.facpeople = rows;
-      res.render("index", { title: 'HR page',
-                          formdata: req.body,
-                          facpeople: rows
-                        })
+      req.app.locals.facpeople = rows;
+      req.app.locals.title = 'HR page';
+      renderPage(req, res, next);
     })
   }
   else {
-    res.render('index', { title: '305demo',
-                          formdata: req.body });
+    req.app.locals.title = '305demo';
+    renderPage(req, res, next);
   }
 }
 
+function renderPage(req, res, next) {
+  res.render('index', { title: req.app.locals.title,
+                        formdata: req.body,
+                        termslist: req.app.locals.termslist,
+                        facpeople: req.app.locals.facpeople                        
+  });
+}
 
 module.exports = router;
